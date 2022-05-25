a3c6df9f0e26d3a52ca976a15c46ac59b97f256a
Quentin Ikeno <ikenoq@gmail.com>
2022-05-24 19:13:41 -1000

    Fill in get method.

--- models/user.js
+++ models/user.js
@@ -91,7 +91,20 @@ class User {
 	 *          join_at,
 	 *          last_login_at } */
 
-	static async get(username) {}
+	static async get(username) {
+		const results = await db.query(
+			`
+    SELECT username, first_name, last_name, phone, join_at, last_login_at
+    FROM users
+    WHERE username=$1
+    `,
+			[username]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError("No user with that username found", 400);
+		}
+		return results.rows[0];
+	}
 
 	/** Return messages from this user.
 	 *

67e7cb98d257a4995112beb7d5f5f8fdbd9a8819
Quentin Ikeno <ikenoq@gmail.com>
2022-05-24 19:14:54 -1000

    Fill in all method.

--- models/user.js
+++ models/user.js
@@ -80,7 +80,15 @@ class User {
 	/** All: basic info on all users:
 	 * [{username, first_name, last_name, phone}, ...] */
 
-	static async all() {}
+	static async all() {
+		const results = await db.query(
+			`
+    SELECT username, first_name, last_name, phone
+    FROM users
+    `
+		);
+		return results.rows;
+	}
 
 	/** Get: get user by username
 	 *

032b1d1849ef93caa1480acdb23eb8df1c798205
Quentin Ikeno <ikenoq@gmail.com>
2022-05-25 17:36:59 -1000

    Create /login and /register auth routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+
+const router = new express.Router();
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.authenticate(username, password);
+		const token = jwt.sign({ username: user.username }, SECRET_KEY);
+		return res.json({ token });
+	} catch (error) {
+		return next(error);
+	}
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +28,22 @@
  *
  *  Make sure to update their last-login!
  */
+router.post("/register", async (req, res, next) => {
+	try {
+		const user = await User.register(req.body);
+		if (user) {
+			const token = jwt.sign({ username: user.username }, SECRET_KEY);
+			return res.json({ token });
+		}
+		throw new ExpressError("Unable to register.  Please try again.", 500);
+	} catch (error) {
+		if (error.code === "23505") {
+			return next(
+				new ExpressError("Username taken. Please pick another!", 400)
+			);
+		}
+		return next(error);
+	}
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,28 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = new express.Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", async (req, res, next) => {
+	try {
+		const users = await User.all();
+		return res.json(users);
+	} catch (error) {
+		return next(error);
+	}
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
 
 /** GET /:username/to - get messages to user
  *
@@ -22,7 +34,6 @@
  *
  **/
 
-
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +43,5 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+module.exports = router;
